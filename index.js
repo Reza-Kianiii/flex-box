59c1c3c92a94b5a421fd0e7464f3c49d98720d92
reza kiani <rezakiani05555@gmail.com>
2023-04-19 22:55:35 -0700

    change new flex-box

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-let sidnav=document.querySelector('.Contianer_navbar');
-
+let sidnav=document.querySelector('.Contianer_navbar_mobile');
+let iconsidnav=document.querySelector('.Contianer_navbar > i')
+let sidnvae_open=document.querySelector('.sidnav-open');
 function manage_menu(){
-    
-    
-    sidnav.classList.remove('sidnav-close')
-        sidnav.classList.add('sidnav-open');
-   
+    console.log(sidnav)
+    sidnav.classList.remove('Contianer_navbar_mobile')
+     sidnav.classList.add('sidnav-open')
+       
 }
 
 
 function close_sidnav(){
-    sidnav.classList.remove('sidnav-open')
-    sidnav.classList.add('sidnav-close')
+sidnav.classList.remove('sidnav-open');
+sidnav.classList.add('Contianer_navbar_mobile');
+    // sidnvae_open.classList.remove('sidnav-opne');
+// sidnvae_open.classList.add('Contianer_navbar_mobile');
+ 
 }
